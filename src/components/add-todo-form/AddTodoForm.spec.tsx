ef3b12248f6ba955f280880bc9f41a57dbe8908c
henryvanner <henry.vanner@gmail.com>
2023-05-08 22:58:14 -0500

    test: update test formatting

--- src/components/add-todo-form/AddTodoForm.spec.tsx
+++ src/components/add-todo-form/AddTodoForm.spec.tsx
@@ -10,6 +10,5 @@ test('The form has a text input for entering the todo description', () => {
 
 test('The form has a button for submiting the new todo', () => {
   render(<AddTodoForm />)
-
   expect(screen.getByRole('button', { name: 'Add' })).toBeInTheDocument()
 })
