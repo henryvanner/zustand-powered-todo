cb42f88a034719be06e4f0e60ad7f7c1400a7711
henryvanner <henry.vanner@gmail.com>
2023-05-09 20:15:55 -0500

    refactor: make AddTodoForm stateless

--- src/components/add-todo-form/AddTodoForm.tsx
+++ src/components/add-todo-form/AddTodoForm.tsx
@@ -1,22 +1,19 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 import { useAddTodo } from '../../hooks'
 import styles from './AddTodoForm.module.css'
 
 export const AddTodoForm: React.FC = () => {
-  const [textInput, setTextInput] = useState('')
+  const textInputRef = useRef<HTMLInputElement>(null)
   const addTodo = useAddTodo()
-
-  const handleTextInputChange: React.ChangeEventHandler<HTMLInputElement> = (
-    event,
-  ) => {
-    setTextInput(event.target.value)
-  }
 
   const handleFormSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault()
 
-    addTodo(textInput)
-    setTextInput('')
+    if (!textInputRef.current) return
+
+    addTodo(textInputRef.current.value)
+    textInputRef.current.value = ''
+    textInputRef.current?.focus()
   }
 
   return (
@@ -32,12 +29,11 @@ export const AddTodoForm: React.FC = () => {
       </label>
       <input
         id='newTodoInput'
+        ref={textInputRef}
         className={styles.input}
         type='text'
         required
         autoFocus
-        value={textInput}
-        onChange={handleTextInputChange}
       />
       <button
         className={styles.button}
