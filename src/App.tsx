775b3e0c9f39d9ece95c44800090aaa040d8d305
henryvanner <henry.vanner@gmail.com>
2023-05-09 21:01:09 -0500

    fix: remove obsolete prop

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,7 @@ export const App = () => {
   return (
     <>
       <AddTodoForm />
-      <TodoList
-        todos={[
-          { id: '1', description: 'make a cup of coffee', completed: true },
-          { id: '2', description: 'run 5 kilometers', completed: false },
-        ]}
-      />
+      <TodoList />
     </>
   )
 }
